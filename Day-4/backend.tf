a74d757a90f6e2e2fe826ae2ce5e7d88f8421746
malliarjun <thatikondamalli4u@gmail.com>
2023-12-09 22:52:09 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
+    bucket         = "mallik-s3-demo-xyz" # change this
+    key            = "malli/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
